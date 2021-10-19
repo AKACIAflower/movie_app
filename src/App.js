37b081f67aad5474d9b9afbfca54d723f84b4178
AKACIAflower <lgs2722@gmail.com>
2021-10-19 22:13:11 +0900

    Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({ favorite }) {
+  // {favorite} => props.favorite 도 가능
+  return <h3>I like {favorite}</h3>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello!! BYE
+      <Food favorite="kimchi" />
+      <Food favorite="ramen" />
+      <Food favorite="chicken" />
     </div>
   );
 }
