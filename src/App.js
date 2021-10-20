645390ad20f4c6511b53dca5a767e11e974da144
AKACIAflower <lgs2722@gmail.com>
2021-10-20 11:04:10 +0900

    JSX-Props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ favorite }) {
+function Fruit({ name, rating }) {
   // {favorite} => props.favorite 도 가능
-  return <h3>I like {favorite}</h3>;
+  return (
+    <div>
+      <h3>I like {name}</h3>
+      <h4>{rating}/5.0</h4>
+    </div>
+  );
 }
+
+Fruit.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
+
+const fruitILike = [
+  { name: "Apple", id: 1, rating: 5.0 },
+  { name: "Banana", id: 2, rating: 3.5 },
+  { name: "Pear", id: 3, rating: 4.6 },
+];
 
 function App() {
   return (
     <div>
-      Hello!! BYE
-      <Food favorite="kimchi" />
-      <Food favorite="ramen" />
-      <Food favorite="chicken" />
+      {fruitILike.map((dish) => (
+        <Fruit key={dish.id} name={dish.name} rating={dish.rating} />
+      ))}
     </div>
   );
 }
